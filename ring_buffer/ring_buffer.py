911749e98765074eefe0e165ad1a326460848341
Kayla <6lackmacaroon@gmail.com>
2019-09-20 11:19:24 -0600

    working out append

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,9 +5,15 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
+    #if storage length < capacity, add the item
+    if len(self.storage) < self.capacity:
+      self.storage[self.current] = item
+    #while current < capacity-1
+    if self.current < self.capacity -1:
+      print("yo")
     #adds element to the buffer
-    #if buffer is full, remove the oldest value
-    pass
+    #if buffer is full, remove the oldest value (fifo)
+    
 
   def get(self):
     #return list of buffer elements in their given order
