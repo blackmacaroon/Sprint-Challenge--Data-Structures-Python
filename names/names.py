1de170da9fbc3a5ea1153c8b9fdeff92df73a1f3
Kayla <6lackmacaroon@gmail.com>
2019-09-20 12:02:33 -0600

    mvp

--- names/names.py
+++ names/names.py
@@ -11,15 +11,21 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 #if name in namesOne, and same name in namesTwo
+# what import helps here...
 duplicates = []
-for name_1 in names_1:
-    if name_1 in names_2:
-        duplicates.append(name_1)
+for name in names_1:
+    if name in names_2:
+        duplicates.append(name)
+#OG runtime: 4.99112606048584 seconds
+#improved runtime: runtime: 1.1109709739685059 seconds
+
+
+# duplicates = set(names_1) & set(names_2)
+# this is why we use built-ins: runtime: 0.004230022430419922 seconds!!!!!!!!!!!!!!!!!!!!!!!!!
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
-#OG runtime: 4.99112606048584 seconds
 
-#improved runtime: runtime: 1.1109709739685059 seconds
