16a91c5b11185d2ef37ca2b4feab69bc61da9f1e
Kayla <6lackmacaroon@gmail.com>
2019-09-20 09:26:00 -0600

    understand the problem - pseudocode

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,10 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
+    #adds element to the buffer
+    #if buffer is full, pop the oldest value
     pass
 
   def get(self):
+    #return list of buffer elements in their given order
     pass
